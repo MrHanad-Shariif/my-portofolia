fc956f2e914d0e4f1ad2e6b0782f702e82da935a
MrHanad-Shariif <hanadshariif18@gmail.com>
2025-06-10 16:13:55 +0300

    change made

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -11,27 +11,27 @@ const socialLinks = [
 	{
 		id: 1,
 		icon: <FiGlobe />,
-		url: 'https://www.stoman.me/',
+		url: 'https://www.hanad.dev/',
 	},
 	{
 		id: 2,
 		icon: <FiGithub />,
-		url: 'https://github.com/realstoman',
+		url: 'https://github.com/MrHanad-Shariif',
 	},
 	{
 		id: 3,
 		icon: <FiTwitter />,
-		url: 'https://twitter.com/realstoman',
+		url: 'https://twitter.com/MrHanadShariif',
 	},
 	{
 		id: 4,
 		icon: <FiLinkedin />,
-		url: 'https://www.linkedin.com/in/realstoman',
+		url: 'https://www.linkedin.com/in/hanad-sharif-412388257',
 	},
 	{
 		id: 5,
 		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/c/realstoman',
+		url: 'https://www.youtube.com/c/MrHanadShariif',
 	},
 ];
 
